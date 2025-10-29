0d5a9cd2178e641f2ba01be8298974328f998bec
stalstierna <andrea.stalstierna@chasacademy.se>
2025-10-29 17:16:49 +0100

    .

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,14 +23,17 @@ app.use("/tasks", taskRoutes);
 app.use("/users", userRoutes);
 app.use("/projects", projectRoutes);
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-await connectDB()
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log("Error connecting to MongoDB");
-  });
+// await connectDB()
+//   .then(() => {
+//     app.listen(PORT, () => {
+//       console.log(`Server running on port ${PORT}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.log("Error connecting to MongoDB");
+//   });
+await connectDB;
+
+export default app;
